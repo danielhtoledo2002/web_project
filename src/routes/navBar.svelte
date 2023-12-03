d7aa95ec22d8e46d64885d5e31fad8a8a31ffb0f
Daniel <0243179@up.edu.mx>
2023-12-03 00:51:37 -0600

    add wrape

--- src/routes/navBar.svelte
+++ src/routes/navBar.svelte
@@ -1,10 +1,10 @@
-<main class="bg-[#A0E9FF] flex flex-col justify-center items-center rounded-lg shadow-2xl mx-5 flex-grow">
-    <nav class="flex items-center justify-between w-full p-4">
-      <div class="flex items-center text-black">
+<main class="bg-[#A0E9FF] flex flex-col justify-center items-center rounded-lg shadow-2xl mx-5 flex-grow flex-wrap">
+    <nav class="flex items-center justify-between w-full p-4 flex-wrap">
+      <div class="flex items-center text-black flex-wrap">
         <a href="./">        <h1 class="text-4xl font-bold text-[#FFF]"><i class="fa-solid fa-cloud"></i></h1></a>
         <span class="font-semibold text-xl tracking-tight ml-2">ListaKLista</span>
       </div>
-      <div >
+      <div class="flex flex-wrap" >
         <a href="./user">
             <h1 class="text-4xl font-bold text-black"><i class="fa-solid fa-user"></i></h1>
         </a>
