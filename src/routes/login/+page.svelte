b3a1736a9f7a41b0b8cd4a5be3d54516c2722ca7
Lorenzo Reinoso Fuentes <0212511@up.edu.mx>
2023-12-02 23:01:41 -0600

    First stable Idea

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,33 @@
+<main class="bg-white max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
+    <section>
+        <h3 class="font-bold text-2xl">Bienvenido a ListaKLista</h3>
+        <p class="text-gray-600 pt-2">Accede a tus Listas</p>
+    </section>
+
+    <section class="mt-10">
+        <form class="flex flex-col" method="POST" action="#">
+            <div class="mb-6 pt-3 rounded bg-gray-200">
+                <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="email">Email/Usuario</label>
+                <input type="text" id="email" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3">
+            </div>
+            <div class="mb-6 pt-3 rounded bg-gray-200">
+                <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="password">Contraseña</label>
+                <input type="password" id="password" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3">
+            </div>
+            <div class="flex justify-end">
+                <a href="#" class="text-sm text-purple-600 hover:text-purple-700 hover:underline mb-6">Olvidaste tu contraseña?</a>
+            </div>
+            <button class="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200" type="submit">Ingresar</button>
+        </form>
+    </section>
+</main>
+
+<div class="max-w-lg mx-auto text-center mt-12 mb-6">
+    <p class="text-white">No tienes una cuenta? <a href="#" class="font-bold hover:underline">Sign up</a>.</p>
+</div>
+
+<footer class="max-w-lg mx-auto flex justify-center text-white">
+    <a href="#" class="hover:underline">Contact</a>
+    <span class="mx-3">•</span>
+    <a href="#" class="hover:underline">Privacy</a>
+</footer>
