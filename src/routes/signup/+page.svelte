2b89f008da48552676c54a24f8f061260833d3cf
Lorenzo Reinoso Fuentes <0212511@up.edu.mx>
2023-12-02 23:25:23 -0600

    Sign Up Creation

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -1,18 +1,43 @@
-<section class="bg-gray-900/90 p-12 rounded-sm flex flex-col gap-10 text-center">
-    <h1 class="text-xl">Sign Up</h1>
-    <form class="flex flex-col gap-4">
-        <input type="text" id="email" name="email" placeholder="Email" class="bg-black/50 px-4 py-2 rounded-sm border-none focus:outline outline-blue-600 outline-offset-2 outline-2">
-        <input type="password" id="password" name="password" placeholder="Password" class="bg-black/50 px-4 py-2 rounded-sm border-none focus:outline outline-blue-600 outline-offset-2 outline-2">
-        <input type="password-reenter" id="password-reenter" name="password-reenter" placeholder="Re-enter Password" class="bg-black/50 px-4 py-2 rounded-sm border-none focus:outline outline-blue-600 outline-offset-2 outline-2">
-        <span class="text-left">
-            <input type="checkbox" id="auto-signin" name="auto-signin">
-            <label for="auto-signin" class="text-sm">Automatically Sign In</label>
-            <br>
-            <input type="checkbox" id="remember-me" name="remember-me">
-            <label for="remember-me" class="text-sm">Remember me</label>
-        </span>
-        <input type="submit" value="Create Account" class="bg-sky-600 p-2 rounded-sm hover:cursor-pointer mt-6">
-    </form>
-    <p class="text-xs">By creating an account you agree to be <br> emailed at the adress provided for verification.</p>
-    <p class="text-sm">Already have an account? <a href="/signin" class="text-sky-600">Sign In</a> here.</p>
-</section>
+<header class="max-w-lg mx-auto">
+    <a href="#">
+        <h1 class="text-6xl font-bold text-white text-center pt-10"><i class="fa-solid fa-cloud"></i></h1>
+    </a>
+</header>
+<main class="bg-[#A0E9FF] max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
+    <section>
+        <h3 class="font-bold text-2xl">Bienvenido a ListaKLista</h3>
+        <p class="text-gray-600 pt-2">Crea tu Cuenta</p>
+    </section>
+
+    <section class="mt-10">
+        <form class="flex flex-col" method="POST" action="#">
+            <div class="mb-6 pt-3 rounded-lg bg-gray-200">
+                <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="email">Tu Rockstar ID</label>
+                <input type="text" id="user" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-blue-600 transition duration-500 px-3 pb-3">
+            </div>
+            <div class="mb-6 pt-3 rounded-lg bg-gray-200">
+                <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="email">Tu medio de confirmación</label>
+                <input type="text" id="email" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-blue-600 transition duration-500 px-3 pb-3">
+            </div>
+            <div class="mb-6 pt-3 rounded-lg bg-gray-200">
+                <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="password">Contraseña</label>
+                <input type="password" id="password" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-blue-600 transition duration-500 px-3 pb-3">
+            </div>
+            <div class="mb-6 pt-3 rounded-lg bg-gray-200">
+                <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="password">Confirma tu Contraseña</label>
+                <input type="password" id="password_2" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-blue-600 transition duration-500 px-3 pb-3">
+            </div>
+            <button class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200" type="submit">Crear</button>
+        </form>
+    </section>
+</main>
+
+<div class="max-w-lg mx-auto text-center mt-12 mb-6">
+    <p class="text-[#00A9FF]">Ya tienes cuenta? <a href="login" class="font-bold hover:underline text-[#00A9FF]">Log In</a>.</p>
+</div>
+
+<footer class="max-w-lg mx-auto flex justify-center text-white">
+    <a href="#" class="hover:underline">Contact</a>
+    <span class="mx-3">•</span>
+    <a href="#" class="hover:underline">Privacy</a>
+</footer>
