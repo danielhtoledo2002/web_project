a2ff60fa793445b37123675dee089f8a5875f06d
Daniel <0243179@up.edu.mx>
2023-12-03 16:23:52 -0600

    create task and tags component

---
+++ src/routes/list.svelte
@@ -0,0 +1,13 @@
+<li class="flex p-9 bg-[#A0E9FF] rounded-lg text-white-400  space-x-1 justify-center">
+    <div class="">
+        <p class="text-opacity-50 font-bold text-black space-x-5 ">NOMBRE</p>
+        <ul class="space-x-5">
+            <i class=" fa-solid fa-pen-to-square"></i>
+            <i class=" fa-solid fa-trash"></i>
+            <i class=" fa-solid fa-play"></i>
+          </ul>
+
+    </div>
+
+
+</li>
--- src/routes/listas/+page.svelte
+++ src/routes/listas/+page.svelte
@@ -3,9 +3,11 @@
     import NavBar from '../navBar.svelte';
     import { connect, session } from '$lib/session.js';
     import { get } from 'svelte/store';
+    import List from '../list.svelte';
 
     let nombre = "";
     let apellido = "";
+    
 
     onMount(async () => {
         try {
@@ -21,10 +23,35 @@
             console.log(err);
             window.location.href = '/login';
         }
+
+
     });
+ 
 </script>
 
 <div class=" flex flex-row grow">
     <NavBar nombre={nombre} apellido={apellido}/>
 </div>
 
+
+<div class="p-9 font-semibold tracking-tight ml-2">
+    <h1>Bienvenido</h1>
+</div>
+
+<div class="p-9">
+    <div class=" p-6  h-12 rounded-lg bg-slate-100 flex flex-row w-64 space-x-5 justify-items-center content-center justify-center items-center">
+        <div>
+            <i class="fa-solid fa-plus"></i>
+        </div>
+        <div>
+            Create Task
+        </div>
+    
+    </div>
+</div>
+
+
+<div class=" grid grid-cols-4  flew-wrape  justify-center gap-4 p-6">
+    <List />
+
+</div>
