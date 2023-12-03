9a6191ff6e11eada1573f48c27dc385861dd6890
Lolo <0212511@up.edu.mx>
2023-12-02 23:34:32 -0600

    Recuperar contraseña y minor changes

--- src/routes/recuperar/+page.svelte
+++ src/routes/recuperar/+page.svelte
@@ -6,31 +6,24 @@
 <main class="bg-[#A0E9FF] max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl">
     <section>
         <h3 class="font-bold text-2xl">Bienvenido a ListaKLista</h3>
-        <p class="text-gray-600 pt-2">Accede a tus Listas</p>
+        <p class="text-gray-600 pt-2">Recuera tu contraseña</p>
     </section>
 
     <section class="mt-10">
         <form class="flex flex-col" method="POST" action="#">
             <div class="mb-6 pt-3 rounded-lg bg-gray-200">
-                <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="email">Email/Usuario</label>
+                <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="email">Correo de Registro</label>
                 <input type="text" id="email" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-blue-600 transition duration-500 px-3 pb-3">
             </div>
-            <div class="mb-6 pt-3 rounded-lg bg-gray-200">
-                <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="password">Contraseña</label>
-                <input type="password" id="password" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-blue-600 transition duration-500 px-3 pb-3">
-            </div>
-            <div class="flex justify-end">
-                <a href="recuperar" class="text-sm text-blue-600 hover:text-blue-700 hover:underline mb-6">Olvidaste tu contraseña?</a>
-            </div>
-            <button class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200" type="submit">Ingresar</button>
+            
+            <button class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200" type="submit">Recuperar</button>
         </form>
     </section>
 </main>
 
 <div class="max-w-lg mx-auto text-center mt-12 mb-6">
-    <p class="text-[#00A9FF]">No tienes una cuenta? <a href="signup" class="font-bold hover:underline text-[#00A9FF]">Sign up</a>.</p>
+    <p class="text-[#00A9FF]">Te acordaste? <a href="login" class="font-bold hover:underline text-[#00A9FF]">Log In</a>.</p>
 </div>
-
 <footer class="max-w-lg mx-auto flex justify-center text-white">
     <a href="mailto:ListKlista@gmail.com" class="hover:underline text-[#89CFF3]">Contacto</a>
     <span class="mx-3">•</span>
--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -12,7 +12,7 @@
     <section class="mt-10">
         <form class="flex flex-col" method="POST" action="#">
             <div class="mb-6 pt-3 rounded-lg bg-gray-200">
-                <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="email">Tu Rockstar ID</label>
+                <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="user">Tu Rockstar ID</label>
                 <input type="text" id="user" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-blue-600 transition duration-500 px-3 pb-3">
             </div>
             <div class="mb-6 pt-3 rounded-lg bg-gray-200">
