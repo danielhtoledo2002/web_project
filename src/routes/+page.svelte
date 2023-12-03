f1911e24a74d9bf879df1ab30a6b41c37e207802
Alejandro Osornio <50227494+AOx0@users.noreply.github.com>
2023-12-02 22:02:39 -0600

    Init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { Surreal } from 'surrealdb.js';
+    const db = new Surreal();
+
+    async function crear() {
+    }
+</script>
+
+<h1>Welcome to SvelteKit!!!</h1>
+<p>Visit <a class="font-bold" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button on:click={crear}>Crear!</button>
