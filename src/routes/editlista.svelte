26e4f3fa34e60080768786a29f3eef6f3f24ca16
Daniel <0243179@up.edu.mx>
2023-12-03 22:07:29 -0600

    component edit lista

---
+++ src/routes/editlista.svelte
@@ -0,0 +1,28 @@
+<script>
+    
+</script>
+
+
+<div class="max-w-md mx-auto my-10 bg-white p-6 rounded-lg shadow-inner">
+    <div>
+      <div class="flex items-start">
+        <h1 class="text-2xl font-bold mb-6">Editar Lista de Tareas</h1>
+        <button class="ml-auto" >
+          <i class="fa-solid fa-times"></i>
+        </button>
+      </div>
+      <div class="mb-4">
+        <label for="nombre-tarea" class="block text-gray-700 font-bold mb-2">Editar el nombre  de la tarea:</label>
+        <input type="text" id="nombre-tarea" placeholder="Ingrese el nombre de la lista de tareas" required
+                class="w-full border-gray-300 rounded-md focus:outline-none focus:border-blue-500 px-4 py-2">
+      </div>
+  
+      <div class="mb-4">
+        <label for="descripcion-tarea" class="block text-gray-700 font-bold mb-2">Editar descripción:</label>
+        <textarea id="descripcion-tarea"  placeholder="Ingrese la descripción de la lista de tareas" 
+                  class="w-full border-gray-300 rounded-md focus:outline-none focus:border-blue-500 px-4 py-2"></textarea>
+      </div>
+  
+      <button class="bg-[#89CFF3] text-black px-4 py-2 rounded-md hover:bg-gray-300" on:click={createList}>Modificar Lista</button>
+    </div>
+  </div>
