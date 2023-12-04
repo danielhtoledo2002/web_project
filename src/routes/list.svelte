b65099bd87e33d88f5a9f0590531f236b9e9aff2
Alejandro Osornio <50227494+AOx0@users.noreply.github.com>
2023-12-03 19:26:23 -0600

    Improve lista

--- src/routes/list.svelte
+++ src/routes/list.svelte
@@ -14,10 +14,10 @@
 <div class="flex p-5 bg-[#A0E9FF] rounded-lg text-white-400  space-x-1 justify-center group">
     <div class="flex flex-col items-center gap-2">
         <p class="text-opacity-50 font-bold text-black space-x-5 ">{nombre}</p>
-        <div class="hidden flex space-x-5 flex-row group-hover:block">
-            <i class="fa-solid fa-pen-to-square"></i>
+        <div class="invisible flex space-x-5 text-black text-opacity-50 flex-row group-hover:visible">
+            <button class="hover:opacity-70 fa-solid fa-pen-to-square"></button>
             <button class="hover:opacity-70 fa-solid fa-trash" on:click={deleteList}></button>
-            <i class="fa-solid fa-play"></i>
+            <button class="hover:opacity-70 fa-solid fa-play"></button>
         </div>
     </div>
 </div>
