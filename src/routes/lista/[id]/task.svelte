a818b00f474ffce93d00ebf0795d7ef8eda129af
Alejandro Osornio <50227494+AOx0@users.noreply.github.com>
2023-12-03 21:41:31 -0600

    Sort elements

--- src/routes/lista/[id]/task.svelte
+++ src/routes/lista/[id]/task.svelte
@@ -2,29 +2,39 @@
     import { db } from '$lib/session.js';
 
     export let tarea;
+    let tarea_orig = tarea.completed;
 
     async function deleteTask() {
         await db.delete(tarea.id);
         window.location.href = '/lista/' + tarea.task_list;
     }
+
+    async function updateCompleted() {
+        await db.merge(tarea.id, { completed: tarea.completed });
+        window.location.href = '/lista/' + tarea.task_list;
+    }
+
+    $: if (tarea_orig != tarea.completed) { updateCompleted() };
 </script>
 
-<div class="flex w-full bg-white rounded-xl p-3 flex-col">
-    <div class={ tarea.due_date_passed ? 'flex justify-between flex-row text-sm text-red-500' : 'flex justify-between flex-row text-sm text-black' }>
-        <div class="flex flex-row">
-            <input type="checkbox" class="mr-2 form-checkbox rounded-full border-gray-300 ">
-            <p>{tarea.due_date_passed ? 'Expired' : tarea.remain.split('m')[0] + 'm' }</p>
+<div class={ tarea.completed ? 'line-through opacity-50' : 'opacity-100' }>
+    <div class="flex w-full bg-white rounded-xl p-3 flex-col">
+        <div class={ tarea.due_date_passed ? 'flex justify-between flex-row text-sm text-red-500' : 'flex justify-between flex-row text-sm text-black' }>
+            <div class="flex flex-row">
+                <input type="checkbox" bind:checked={tarea.completed} class="mr-2 form-checkbox rounded-full border-gray-300 ">
+                <p>{tarea.due_date_passed ? 'Expired' : tarea.remain.split('m')[0] + 'm' }</p>
+            </div>
+            <p>{tarea.due_date_format}</p>
         </div>
-        <p>{tarea.due_date_format}</p>
+        <div class="flex flex-row justify-between">
+            <div>
+                <p class="font-bold">{tarea.name}</p>
+            </div>
+            <div class="flex flex-row gap-3">
+                <button class="hover:opacity-70 fa-solid fa-pen-to-square"></button>
+                <button class="hover:opacity-70 fa-solid fa-trash" on:click={deleteTask}></button>
+            </div>
+        </div>
+        <p class="text-sm">Descripción: {tarea.description}</p>
     </div>
-    <div class="flex flex-row justify-between">
-        <div>
-            <p class="font-bold">{tarea.name}</p>
-        </div>
-        <div class="flex flex-row gap-3">
-            <button class="hover:opacity-70 fa-solid fa-pen-to-square"></button>
-            <button class="hover:opacity-70 fa-solid fa-trash" on:click={deleteTask}></button>
-        </div>
-    </div>
-    <p class="text-sm">Descripción: {tarea.description}</p>
 </div>
