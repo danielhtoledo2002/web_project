1b830ac0edc8c1264475d3a006255f0665761ac6
Alejandro Osornio <50227494+AOx0@users.noreply.github.com>
2023-12-03 21:06:25 -0600

    Changes to task

--- src/routes/lista/[id]/task.svelte
+++ src/routes/lista/[id]/task.svelte
@@ -7,9 +7,18 @@
 </script>
 
 <div class="flex w-full bg-white rounded-xl p-3 flex-col">
-    <div class="flex flex-row gap-3">
-        <p class="font-bold">{tarea.name}</p>
-        <p>{tarea.description}</p>
+    <div class={ tarea.due_date_passed ? 'flex justify-between flex-row text-sm text-red-500' : 'flex justify-between flex-row text-sm text-black' }>
+        <p>{tarea.due_date_passed ? 'Expired' : tarea.remain.split('m')[0] + 'm' }</p>
+        <p>{tarea.due_date_format}</p>
     </div>
-    <p class={ tarea.due_date_passed ? 'text-sm text-red-500' : 'text-sm text-black' }>{tarea.due_date}</p>
+    <div class="flex flex-row justify-between">
+        <div>
+            <p class="font-bold">{tarea.name}</p>
+        </div>
+        <div class="flex flex-row gap-3">
+            <button class="hover:opacity-70 fa-solid fa-pen-to-square"></button>
+            <button class="hover:opacity-70 fa-solid fa-trash" on:click={deleteTask}></button>
+        </div>
+    </div>
+    <p class="text-sm">Description: {tarea.description}</p>
 </div>
