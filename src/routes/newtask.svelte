5c93e47420b3cdf97569b433168ef02c28d90879
Lolo <0212511@up.edu.mx>
2023-12-03 22:47:44 -0600

    Final New task

--- src/routes/newtask.svelte
+++ src/routes/newtask.svelte
@@ -42,36 +42,36 @@
 }
 </style>
 
-<div class="max-w-lg mx-auto my-10 bg-white p-6 rounded-lg shadow-inner">
-<div class="flex items-start">
-    <h1 class="text-2xl font-bold mb-6">Crear Tarea</h1>
-    <button class="close-button pl-6" on:click={closeMenu}>
-    <i class="fa-solid fa-times"></i>
-    </button>
-</div>
-<div class="mb-4">
-    <label for="name" class="block text-gray-700 font-bold mb-2">Nombre de la tarea:</label>
-    <input type="text" id="name" class="input-field" />
-</div>
+<div class="justify-between mx-auto w-1/3 my-10 bg-white p-6 rounded-lg shadow-inner ">
+    <div class="flex justify-between">
+        <h1 class="text-2xl font-bold mb-6">Crear Tarea</h1>
+        <button class="close-button pl-6 h-10" on:click={closeMenu}>
+        <i class="fa-solid fa-times"></i>
+        </button>
+    </div>
+    <div class="mb-4 flex justify-start">
+        <label for="name" class="block text-gray-700 font-bold mb-2 mr-2">Nombre de la tarea:</label>
+        <input type="text" id="name" class="border-2 rounded-lg border-[#6bbce3]" />
+    </div>
 
-<div class="mb-4">
-    <label for="description" class="block text-gray-700 font-bold mb-2">Descripción:</label>
-    <textarea id="description" class="input-field"></textarea>
-</div>
+    <div class="mb-4 flex justify-start">
+        <label for="description" class="block text-gray-700 font-bold mb-2 mr-2">Descripción:</label>
+        <textarea id="description" class="border-2 rounded-lg border-[#6bbce3] border-1 "></textarea>
+    </div>
 
-<div class="mb-4">
-    <label for="dueDate" class="block text-gray-700 font-bold mb-2">Fecha de Entrega:</label>
-    <input type="date" id="dueDate" class="input-field" />
-</div>
+    <div class="mb-4 flex justify-start">
+        <label for="dueDate" class="block text-gray-700 font-bold mb-2 mr-2">Fecha de Entrega:</label>
+        <input type="datetime-local" id="dueDate" class="input-field" />
+    </div>
 
-<div class="mb-4">
-    <label for="priority" class="block text-gray-700 font-bold mb-2">Prioridad:</label>
-    <select id="priority" class="input-field">
-    {#each priorityOptions as option (option)}
-    <option value={option}>{option}</option>
-    {/each}
-    </select>
-</div>
+    <div class="mb-4 flex justify-start">
+        <label for="priority" class="block text-gray-700 font-bold mb-2 mr-2">Prioridad:</label>
+        <select id="priority" class="input-field">
+        {#each priorityOptions as option (option)}
+        <option value={option}>{option}</option>
+        {/each}
+        </select>
+    </div>
 
-<button class="button" on:click={createTask}>Crear Tarea</button>
+    <button class="button" on:click={createTask}>Crear Tarea</button>
 </div>
